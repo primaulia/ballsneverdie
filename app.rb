c6ec01876f9890fa254b28a50964be88835824b6
Prima Aulia Gusta <primaulia@gmail.com>
2021-11-11 16:24:42 +0800

    create teams table

--- app.rb
+++ app.rb
@@ -6,3 +6,10 @@ require "sinatra/activerecord"
 get "/" do
   "Hello world!"
 end
+
+
+# TODO
+# Create DB Visualization between Team and Players - 1 TEAM : N PLAYERS ✅
+# Create DB Visualization between Team and Games - 1 GAME : N TEAMS ✅
+
+# Create Migration for Teams Table
