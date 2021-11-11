035bc622b521d90b0c9b6f75255936dea511ba92
Prima Aulia Gusta <primaulia@gmail.com>
2021-11-11 17:07:01 +0800

    end of reboot day 1

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# TODO: create all teams based on the API given
+
+# call the get all teams API
+# url = "https://www.balldontlie.io/api/v1/teams"
+
+# for each of 30 teams that you get from the JSON
+# create one team in your DB
+
+require 'open-uri'
+require 'json'
+
+# call the api
+  # get results --> see the api response
+  # on each results (30 teams) ==> Team.create!(name: ??)
+
