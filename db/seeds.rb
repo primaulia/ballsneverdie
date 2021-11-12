62495bd980e4ad81a8fc6195dc6c74cacf562440
Prima Aulia Gusta <primaulia@gmail.com>
2021-11-12 12:14:33 +0800

    latest seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,24 @@
 
 require 'open-uri'
 require 'json'
+require 'pry-byebug'
 
+puts "Start of the seed 🏀"
 # call the api
   # get results --> see the api response
   # on each results (30 teams) ==> Team.create!(name: ??)
+url = "https://www.balldontlie.io/api/v1/teams"
+raw_json = open(url).read
+parsed_json = JSON.parse(raw_json)
 
+data = parsed_json['data']
+
+data.each do |team_hash|
+  team_name = team_hash["full_name"]
+  puts "Seeding #{team_name} to the db 🚀"
+  Team.create!(
+    name: team_name
+  )
+end
+
+puts "End of the seeds 🙏"
